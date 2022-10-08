06e588e9f559b8334d8524303d54708429c59ef9
Ksyusha <91797789+pecenikaKsyu@users.noreply.github.com>
2022-10-08 21:32:32 +0300

    Delete EmailNotification.java

--- EmailNotification.java
+++
@@ -1,8 +0,0 @@
-// O- OpenClosed principle
-public class EmailNotification implements NotificationService{
-    @Override
-    public void sendMessage(String message) {
-        //write email
-        //use JavaMailSenderAPI
-    }
-}
