86f1dd1fa01eae587621e0221e711170c9af7aa8
Ksyusha <91797789+pecenikaKsyu@users.noreply.github.com>
2022-10-08 21:33:04 +0300

    Delete MobileNotification.java

--- MobileNotification.java
+++
@@ -1,8 +0,0 @@
-// O- OpenClosed principle
-public class MobileNotification implements NotificationService{
-    @Override
-    public void sendMessage(String message) {
-        //write sms
-        //send sms
-    }
-}
