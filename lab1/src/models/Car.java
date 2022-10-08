3010a97e4a25acaa169deece77d5c3e154e3b79e
Ksyusha <91797789+pecenikaKsyu@users.noreply.github.com>
2022-10-08 21:33:57 +0300

    upload lab1

---
+++ lab1/src/models/Car.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Car {
+    int ID;
+    String carType;
+    String model;
+    String color;
+
+    public Car(int ID, String type, String model, String color) {
+        this.ID = ID;
+        this.carType = type;
+        this.model = model;
+        this.color = color;
+    }
+}
---
+++ lab1/src/models/Client.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Client {
+    String client;
+    int clientID;
+
+    public Client(String client, int clientID) {
+        this.client = client;
+        this.clientID = clientID;
+    }
+}
